1b21bc422086bae16d2033e62710b0e498ec16c2
mikerush1023 <67387773+mikerush1023@users.noreply.github.com>
2020-08-26 09:37:37 -0400

    Create returning-strings.cs

---
+++ returning-strings.cs
@@ -0,0 +1,10 @@
+using System;
+
+public static class Kata
+{
+  public static string Greet(string name)
+  {
+    var greeting = $"Hello, {name} how are you doing today?";
+    return greeting;
+  }
+}
